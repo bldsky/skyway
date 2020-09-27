e3c43db6db6325169c40cba3dd1771daec712a25
Kiuchi Yuya <skylovercoin@gmail.com>
2020-09-28 02:49:22 +0900

    test code by qiita 6

--- room/script.js
+++ room/script.js
@@ -83,6 +83,7 @@ const Peer = window.Peer;
       gainNode = audioContext.createGain();
       sourceAC.connect(gainNode);
       gainNode.connect(audioDestination);
+      gainNode.gain.setValueAtTime(0.5, audioContext.currentTime);
       newVideo.srcObject = audioDestination.stream;
       newVideo.playsInline = true;
       // mark peerId to find it later at peerLeave event
