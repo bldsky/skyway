6f7f3a41461f87e4f9c80304a5884be1c7602aad
bldsky <skylovercoin@gmail.com>
2021-01-12 10:53:42 +0900

    元に戻した

--- room/script.js
+++ room/script.js
@@ -184,7 +184,7 @@ const Peer = window.Peer;
 
       //   room.send([drag.style.left, drag.style.top]);
       // })
-    let client = mqtt.connect('wss://test.mosquitto.org');
+    let client = mqtt.connect('wss://test.mosquitto.org:8081');
     // var mqttOpt = {
     //   clientId: 'testClientId',
     //   username: 'icecream',
